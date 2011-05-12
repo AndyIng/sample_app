8cbba0e64255c61a44a8a8bc14bfaf2c3a395e7b
andrew ingram <ingramag@gmail.com>
2011-05-12 15:54:11 -0700

    end of 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,8 @@ class User < ActiveRecord::Base
          return nil if user.nil?
          return user if user.has_password?(submitted_password)
     end
-
-    private
+     
+     private
 
       def encrypt_password
          self.salt = make_salt if new_record?
