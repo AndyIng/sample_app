c9d715d04db15d1d912a18571ab5269e870958a2
andrew ingram <ingramag@gmail.com>
2011-05-14 13:04:42 -0700

    done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
  resources :users
+ resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
  
 
   root :to => 'pages#home'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -100,7 +100,13 @@ before(:each) do
         it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
-      end   
+      end
+      
+       it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+   
     end
   end
 end
